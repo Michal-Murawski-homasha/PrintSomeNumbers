3ef16281d55c01b8adda0927d35ad5cbf0f5eeb2
Michał Murawski <homasha@gmail.com>
2022-08-18 16:20:14 +0200

    Prints comma after every number except last one

--- src/PrintSomeNumbers.java
+++ src/PrintSomeNumbers.java
@@ -3,6 +3,9 @@ public class PrintSomeNumbers {
         float[] someNumbers= {39, 11, 41, 8, 43, 5, 45, 2, 47, -1, 49, -4, 51, -7, 53, -10, 55, -13, 57, -16};
         for (int i = 0; i < someNumbers.length; i++) {
             System.out.print(someNumbers[i]);
+            if (i < someNumbers.length - 1) {
+                System.out.print(", ");
+            }
         }
     }
 }
