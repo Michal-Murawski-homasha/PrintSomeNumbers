5e2d15d812d0adb825155a3cda5597b71a76e184
Michał Murawski <homasha@gmail.com>
2022-08-18 16:10:07 +0200

    Add table someNumbers

--- src/PrintSomeNumbers.java
+++ src/PrintSomeNumbers.java
@@ -1,5 +1,5 @@
 public class PrintSomeNumbers {
     public static void main(String[] args) {
-
+        float[] someNumbers= {39, 11, 41, 8, 43, 5, 45, 2, 47, -1, 49, -4, 51, -7, 53, -10, 55, -13, 57, -16};
     }
 }
